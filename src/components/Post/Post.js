81aaef038c4823ce3c360e17e8f7f71836804427
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2022-03-02 15:46:00 -0500

    Create Post Component

--- src/components/Post/Post.js
+++ src/components/Post/Post.js
@@ -1,11 +1,22 @@
 import React from 'react';
 import styles from './Post.module.scss';
+import { FaHeart, FaShareAlt } from 'react-icons/fa';
 
 const Post = ({ content, date }) => {
   return (
     <React.Fragment>
       <p className={styles.postsContent}>{content}</p>
-      <p className={styles.postsDate}>{date}</p>
+      <ul className={styles.postsMeta}>
+        <li className={styles.postsMetaData}>
+          <FaHeart />
+          34
+        </li>
+        <li className={styles.postsMetaData}>
+          <FaShareAlt />
+          Share
+        </li>
+        <li className={styles.postsMetaData}>{date}</li>
+      </ul>
     </React.Fragment>
   );
 };
