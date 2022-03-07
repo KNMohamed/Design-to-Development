7051ea6feb0ee9efa048481d0c523dea0f6df15f
yourmaingotoguy <khalid.n.mohamed@outlook.com>
2022-03-07 15:27:56 -0500

    deploying without any posts

--- src/pages/index.js
+++ src/pages/index.js
@@ -75,7 +75,7 @@ export default function Home({ posts: defaultPosts }) {
 }
 
 export async function getStaticProps() {
-  const posts = await getAllPosts();
+  //const posts = await getAllPosts();
   return {
     props: {
       posts,
